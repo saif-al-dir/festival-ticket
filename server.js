7c4fccb905dac86796ddf58002fb50aabe104b5c
saif-al-dir <saif.aldiresee@gmail.com>
2025-06-27 20:02:54 +0000

    add live link to readme

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
@@ -6,30 +5,27 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // Import route files
 const testimonialsRoutes = require("./routes/testimonials.routes");
 const concertsRoutes = require("./routes/concerts.routes");
 const seatsRoutes = require("./routes/seats.routes");
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Use routes with /api prefix
 app.use("/api", testimonialsRoutes);
 app.use("/api", concertsRoutes);
 app.use("/api", seatsRoutes);
 
-// Serve static files from React build
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// Catch-all handler for React Router
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
+app.get("", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 // Start the server
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
